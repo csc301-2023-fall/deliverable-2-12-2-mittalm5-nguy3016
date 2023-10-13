6e5f1e3ca038d85f170042cd93225065f18de39a
ANQ-Nguyen <93494123+ANQ-Nguyen@users.noreply.github.com>
2023-10-12 22:20:53 -0400

    Data from the user is now retrieved by home() and can now be used to call a function that takes care of the cloning.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        dashboard_source = request.form.get('dashboard_source')
+        config = request.form.get('config')
+        destination_name = request.form.get('destination_name')
+
+        # Send to Manyas Function
+
     return render_template("home.html")
