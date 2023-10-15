152089cfd08872c0364b1c70f9e4bdfceb01f3c0
ANQ-Nguyen <93494123+ANQ-Nguyen@users.noreply.github.com>
2023-10-15 19:10:57 -0400

    Mockup HTML frontend that displays all dashboard titles give that the backend provides a dictionary (named data) whose values are the titles.
    Make prettier frontend mockups
    
    Missing editions created by Manya's last pull request as I keep getting error messages as my laptop is not capable of running the proof of concept

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config['SECRET_KEY'] = "AzsdkqjwheqjeaposdApqEpdasb"
 
     from .views import views
--- website/views.py
+++ website/views.py
@@ -5,11 +5,22 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        dashboard_source = request.form.get('dashboard_source')
-        config = request.form.get('config')
-        destination_name = request.form.get('destination_name')
 
-        # Send to Manyas Function
+    data = {
+        'id1': 'Dashboard 1',
+        'id2': 'Dashboard 2',
+        'id3': 'Dashboard 3',
+        'id4': 'Dashboard 4'
+    }
 
-    return render_template("home.html")
+    return render_template("home.html", data=data)
+
+
+@views.route('/clone', methods=['POST'])
+def clone():
+    # Send to Manyas Function
+    dashboard_source = request.form.get('dashboard_source')
+    config = request.form.get('config')
+    destination_name = request.form.get('destination_name')
+
+    return render_template("clone.html")
